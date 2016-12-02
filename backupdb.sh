1350e76be2df078f7b50f6cc6de963850ef7c55e
arsanto <arsanto@users.noreply.github.com>
2016-12-02 23:04:00 +0700

    Create backupdb.sh

---
+++ backupdb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Basic configuration: datestamp e.g. YYYYMMDD
+
+DATE=$(date +"%Y%m%d")
+
+# Location of your backups (create the directory first!)
+
+BACKUP_DIR="/home/db"
+
+# MySQL login details
+
+MYSQL_USER="root"
+MYSQL_PASSWORD="yourpassword"
+
+# MySQL executable locations (no need to change this)
+
+MYSQL=/usr/bin/mysql
+MYSQLDUMP=/usr/bin/mysqldump
+
+# MySQL databases you wish to skip
+
+SKIPDATABASES="Database|information_schema|performance_schema|mysql"
+
+# Number of days to keep the directories (older than X days will be removed)
+
+RETENTION=14
+
+# ---- DO NOT CHANGE BELOW THIS LINE ------------------------------------------
+#
+# Create a new directory into backup directory location for this date
+
+mkdir -p $BACKUP_DIR/$DATE
+
+# Retrieve a list of all databases
+
+databases=`$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "($SKIPDATABASES)"`
+
+# Dumb the databases in seperate names and gzip the .sql file
+
+for db in $databases; do
+echo $db
+$MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --skip-lock-tables --events --databases $db | gzip > "$BACKUP_DIR/$DATE/$db.sql.gz"
+done
+
+# Remove files older than X days
+
+find $BACKUP_DIR/* -mtime +$RETENTION -delete
